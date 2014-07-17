06f8be76c7c0aa3a096ee92079c81c150ec11c11
tmh144429 <tmh144429@gmail.com>
2014-07-16 22:31:17 -0400

    menu page

--- public/dist/application.js
+++ public/dist/application.js
@@ -145,6 +145,9 @@ angular.module('core').config([
     $stateProvider.state('home', {
       url: '/',
       templateUrl: 'modules/core/views/home.client.view.html'
+    }).state('home.menu', {
+      url: 'menu',
+      templateUrl: 'modules/core/views/home.client.view.menu.html'
     });
   }
 ]);'use strict';
@@ -171,6 +174,90 @@ angular.module('core').controller('HomeController', [
   function ($scope, Authentication) {
     // This provides Authentication context.
     $scope.authentication = Authentication;
+    $scope.items = [
+      {
+        name: 'Cherry-Almond Granola ',
+        desc: 'bean Greek yogurt, berries, lemon balm '
+      },
+      {
+        name: 'Hickory & Oak Smoked Pork Belly',
+        desc: 'five minute egg, baby kale, cherry-vanilla bean maple syrup'
+      },
+      {
+        name: 'Grilled Sticky ',
+        desc: 'cr\xe8me anglaze, walnut brittle'
+      },
+      {
+        name: 'Blood Orange Cured Alaskan Salmon',
+        desc: 'pickled onion, local Spring Valley spinach, dill cream cheese, everything bagel'
+      }
+    ];
+    $scope.tastings = [
+      {
+        name: 'Composition of local greens',
+        course: 'First Course',
+        desc: 'local heirloom tomato, parmesan-pepper cracker, broken verjus vinegar + evoo'
+      },
+      {
+        name: 'Ocean garden shrimp + scallop \u201cpotato crusted\u201d',
+        course: 'Second Course',
+        desc: 'roasted peppers, grilled baby eggplant, lemon emulsion'
+      },
+      {
+        name: 'All nature roasted chicken',
+        course: 'Third Course',
+        desc: 'leg risotto, wilted spinach + frisee, pinot noir reduction'
+      },
+      {
+        name: 'Prime hanger steak',
+        course: 'Fourth Course',
+        desc: 'corn grits, local beans, maytag blue cream'
+      },
+      {
+        name: 'Trio of house-made ice cream & lemon pound cake',
+        course: 'Fifth Course',
+        desc: 'coconut, double chocolate-mint, strawberry-basil'
+      }
+    ];
+    $scope.firsts = [
+      {
+        name: 'Composition of local greens',
+        course: 'First Course',
+        desc: 'trilogy of beets, parmesan-pepper cracker, broken verjus vinegar + evoo'
+      },
+      {
+        name: 'Clare Isle organic salmon \u201cblood orange cured\u201d',
+        course: 'First Course',
+        desc: ', endive, \u201ccandy\u201d red onion champagne vinegar'
+      },
+      {
+        name: 'Chicken fired pork belly \u201ccaesar salad\u201d',
+        course: 'First Course',
+        desc: 'romaine, oven dried tomato, peppercorn-parmesan'
+      }
+    ];
+    $scope.seconds = [
+      {
+        name: 'Veal shank',
+        course: 'Second Course',
+        desc: 'sweet potato fingerling + cauliflower hash, kale, reduced braising liquid'
+      },
+      {
+        name: 'Beef short rib',
+        course: 'Second Course',
+        desc: 'corn grits, dandelion greens, buttermilk fired okra \u201cpopper\u201d'
+      },
+      {
+        name: 'Hand-made fresh pasta',
+        course: 'Second Course',
+        desc: 'grilled summer squash, local tomatoes & mushrooms, basil'
+      },
+      {
+        name: 'Local vegetable terrine & wild Alaskan copper river salmon',
+        course: 'Second Course',
+        desc: 'mascarpone, cous cous, pea tendrils, tomato broth'
+      }
+    ];
   }
 ]);'use strict';
 //Menu service used for managing  menus
